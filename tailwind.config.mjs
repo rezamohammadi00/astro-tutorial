f5123d2fc47aef9e3250f3da0e17304fe84f7573
Rez <mohammadireza7979.com>
2024-08-24 01:30:06 +0330

    55

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+	fontFamily:{
+		"alibaba":"Alibaba",
+	  },
+	  fontWeight: {
+		light: 300,
+		regular: 400,
+		bold: 700,
+	  },
+    extend: {},
+  },
+  plugins: [],
+};
